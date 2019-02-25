824fa02640db59c370bfd715cdbbdcd0084e151b
Rooman <Mohammad.zahid96@myhunter.cuny.edu>
2019-02-25 16:23:31 -0500

    completed with google oauth

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose=require("mongoose");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
+const GoogleStrategy=require("passport-google-oauth20").Strategy;
+const findOrCreate=require("mongoose-findorcreate");
 
 // const bcrypt=require("bcrypt");  -->bcrypt
 // const saltRounds=10;  //how many rounds to add salt  bcrypt
@@ -40,12 +42,15 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 mongoose.set("useCreateIndex",true);     //to clear deprecation warning
 const userSchema= new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String,
+  secret:String
 });
 
 //serialize cookies authenticate
 //plugin to hash with passport
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});   -->mongoose encrypt
 //process.env.secret to access the files in our.env file
@@ -55,9 +60,35 @@ userSchema.plugin(passportLocalMongoose);
 
 const User=new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-//serialize users with passport
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//serialize users with passport locally
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user,done){
+  done(null,user.id);
+});
+
+passport.deserializeUser(function(id,done){
+  User.findById(id,function(err,user){
+    done(err,user);
+  })
+});
+
+
+//using oauth
+passport.use(new GoogleStrategy({
+  clientID:process.env.CLIENT_ID,
+  clientSecret:process.env.CLIENT_SECRET,
+  callbackURL:"http://localhost:3000/auth/google/secrets",
+  //used to use google+ but now its deprecated
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},function(accessToken,refreshToken,profile,cb){
+  //findorcreate not real function so install mongoose package to run
+  User.findOrCreate({googleId:profile.id},function(err,user){
+    return cb(err,user);
+  });
+}
+));
 
 //take password add key and get a cipher method
 //hashing no longer needs encryption key, turns password into hash
@@ -70,6 +101,21 @@ app.get("/",function(req,res){
   res.render("home");     //renders home.ejs page
 })
 
+//when the b=signin with google button pressed
+app.get("/auth/google",
+  passport.authenticate("google",{scope:["profile"]})
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google",{failureRedirect:"/login"}),
+  function(req,res){
+    res.redirect("/secrets")
+  }
+)
+
+
+
+
 app.get("/login",function(req,res){
   res.render("login");     //renders home.ejs page
 })
@@ -80,12 +126,43 @@ app.get("/register",function(req,res){
 
 app.get("/secrets",function(req,res){
   //only if authenticated and logged in then show
+  //find all secret fields where not null
+  User.find({"secret":{$ne:null}},function(err,foundUsers){
+    if (err) {
+          console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets",{usersWithSecrets:foundUsers});
+      }
+    }
+  })
+});
+
+
+app.get("/submit",function(req,res){
   if(req.isAuthenticated()){
-      res.render("secrets");
+      res.render("submit");
   } else {
     res.redirect("/login")
   }
 });
+
+app.post("/submit",function(req,res){
+  const submittedSecret=req.body.secret;
+  //from submit ejs name
+  User.findById(req.user.id,function(err,foundUser){
+    if (err) {
+          console.log(err);
+    } else {
+        if (foundUser.secret=submittedSecret) {
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          })
+        }
+    }
+  })
+
+})
 
 //deauthenticate user and log them out with passport
 app.get("/logout",function(req,res){
